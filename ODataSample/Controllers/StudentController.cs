859ad1a30808d92bf9bac602971754ceb257475f
Mukhammadkarim Tukhtaboyev <75162831+MrTukhtaboyev@users.noreply.github.com>
2021-12-07 14:47:52 +0500

    Add project files.

---
+++ ODataSample/Controllers/StudentController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+using ODataSample.Models;
+using System;
+using System.Collections.Generic;
+
+namespace ODataSample.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        [EnableQuery]
+        public ActionResult<List<Student>> GetAllStudents()
+        {
+            return Ok(new List<Student>
+            {
+                new Student
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Muhammadkarim To'xtaboyev",
+                    Grade = 120
+                },
+                new Student
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Javohir Buzrukoaav",
+                    Grade = 100
+                },
+                new Student
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Samandar Uralov",
+                    Grade = 200
+                }
+            });
+        }
+    }
+}
